2b6e583b553df6d4a904226dec73f521e5eae5a4
ricardotulio <ledo.tulio@gmail.com>
2024-07-28 18:21:11 +0100

    Fix start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start the Spring Boot application
 cd /home/ec2-user/ministore-api
-nohup java -jar app.jar > /home/ec2-user/ministore-api.log 2>&1 &
+nohup java -jar target/app.jar > /home/ec2-user/ministore-api.log 2>&1 &
