ed9702ba2be6fe5a6aafc0d3d3cc9472b8821deb
ricardotulio <ledo.tulio@gmail.com>
2024-07-28 18:03:02 +0100

    Fix stop_server.sh script

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-echo "Stopping the Spring Boot application..." > /home/ec2-user/stop_server.log
-pkill -f 'java -jar /home/ec2-user/ministore-api/app.jar' >> /home/ec2-user/stop_server.log 2>&1
-if [ $? -ne 0 ]; then
-  echo "Failed to stop the application" >> /home/ec2-user/stop_server.log
-  exit 1
+
+LOG_FILE=/home/ec2-user/stop_server.log
+APP_JAR="/home/ec2-user/ministore-api/app.jar"
+echo "Checking if the Spring Boot application is running..." > $LOG_FILE
+
+# Check if the application is running
+if pgrep -f "java -jar $APP_JAR" -eq 0 > /dev/null 2>&1; then
+  echo "Application is running. Attempting to stop it..." >> $LOG_FILE
+  pkill -f "java -jar $APP_JAR" >> $LOG_FILE 2>&1
+  if [ $? -ne 0 ]; then
+    echo "Failed to stop the application" >> $LOG_FILE
+    exit 1
+  else
+    echo "Application stopped successfully" >> $LOG_FILE
+    exit 0
+  fi
 else
-  echo "Application stopped successfully" >> /home/ec2-user/stop_server.log
+  echo "Application is not running. No action needed." >> $LOG_FILE
   exit 0
 fi
