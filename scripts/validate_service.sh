32520330f123439be4b49d3eb0a47465aa349d09
ricardotulio <ledo.tulio@gmail.com>
2024-07-28 18:39:14 +0100

    Fix validate_servier.sh file

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -4,7 +4,7 @@ APP_JAR="/home/ec2-user/ministore-api/target/app.jar"
 echo "Validating if the Spring Boot application is running..." > $LOG_FILE
 
 # Check if the application is running
-if pgrep -f "java -jar $APP_JAR" -eq 0 > /dev/null 2>&1; then
+if pgrep -f "java -jar $APP_JAR" > /dev/null 2>&1; then
   echo "Application is running." >> $LOG_FILE
   exit 0
 else
