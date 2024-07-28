7fdff9de50151a0d9d7f00c7b7229a1a2abbacfb
ricardotulio <ledo.tulio@gmail.com>
2024-07-28 17:36:38 +0100

    testing again

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Stop the Spring Boot application
-pkill -f 'java -jar app.jar'
+echo "Stopping the Spring Boot application..." > /home/ec2-user/stop_server.log
+pkill -f 'java -jar /home/ec2-user/ministore-api/app.jar' >> /home/ec2-user/stop_server.log 2>&1
+if [ $? -ne 0 ]; then
+  echo "Failed to stop the application" >> /home/ec2-user/stop_server.log
+  exit 1
+else
+  echo "Application stopped successfully" >> /home/ec2-user/stop_server.log
+  exit 0
+fi
